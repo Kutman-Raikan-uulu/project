1dfdfed63801521deff5bafae84e4639ac0a1aff
Kutman Raikan uulu <rajkanuulu@gmail.com>
2019-05-07 14:03:08 +0600

    rename

---
+++ src/Content/Content.js
@@ -0,0 +1,9 @@
+import React from "react";
+import What from "./What/What";
+import classes from "./Content.module.css";
+
+function Content(props) {
+  return <div className={classes} />;
+}
+
+export default Content;
